f123a0daa704bb25a352d39c0ff1401ab197e4a1
Emily Byrne <embyrne13@gmail.com>
2023-01-08 11:31:23 -0500

    form submits

--- form/src/App.js
+++ form/src/App.js
@@ -1,29 +1,80 @@
 import './App.css'
+import React from 'react'
+import { useState } from 'react'
 
 function App() {
+  const [formValues, setFormValues] = useState({
+    name: '',
+    email: '',
+    password: '',
+    occupation: '',
+    state: ''
+  })
+  const handleChange = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value })
+    e.preventDefault()
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setFormValues({
+      name: '',
+      email: '',
+      password: '',
+      occupation: '',
+      state: ''
+    })
+  }
   return (
     <div className="App">
       <header className="App-header">Please complete this form.</header>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-input">
           <label>name: </label>
-          <input name="name" type="text" placeholder="Full Name" />
+          <input
+            onChange={handleChange}
+            name="name"
+            type="text"
+            placeholder="Full Name"
+            value={formValues.name}
+          />
         </div>
         <div className="form-input">
           <label>email: </label>
-          <input name="email" type="email" placeholder="email" />
+          <input
+            onChange={handleChange}
+            name="email"
+            type="email"
+            placeholder="email"
+            value={formValues.email}
+          />
         </div>
         <div className="form-input">
           <label>password: </label>
-          <input type="password" name="password" placeholder="*****" />
+          <input
+            onChange={handleChange}
+            type="password"
+            name="password"
+            placeholder="*****"
+            value={formValues.email}
+          />
         </div>
         <div className="form-input">
           <label>occupation: </label>
-          <input name="occupation" placeholder="occupation" />
+          <input
+            onChange={handleChange}
+            name="occupation"
+            placeholder="occupation"
+            value={formValues.occupation}
+          />
         </div>
         <div className="form-input">
           <label>state: </label>
-          <input name="state" placeholder="state" />
+          <input
+            onChange={handleChange}
+            name="state"
+            placeholder="state"
+            value={formValues.state}
+          />
         </div>
         <div className="form button">
           <button type="submit">Submit form</button>
